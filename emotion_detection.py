915f18ce1b1eeb6afacc02c9cb867fbc6cac08e6
nickdenny <123418391+nickdenny@users.noreply.github.com>
2024-06-23 21:29:37 -0700

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,4 +7,5 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    return formatted_response['emotionPredictions']['emotion']['anger']
+    print(formatted_response)
+    # return formatted_response['emotionPredictions']['emotion']['anger']
